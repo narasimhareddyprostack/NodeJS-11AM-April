fce85c0ef13507773eda1c5913a76d7d211b1ce2
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2024-05-08 15:27:21 +0530

    tst

--- ProductCRUD/routes/productRouter -4.js
+++ ProductCRUD/routes/productRouter -4.js
@@ -2,50 +2,8 @@ import express from 'express'
 import Product from '../model/Product.js'
 let router = express.Router()
 
-
-/*
-    URL: http://127.0.0.1:8080/products/create
-    Method Type:POST
-    Required Fields: name,price,qty,info,image
-*/
-router.post("/create", async(req,resp)=>{
-    let newProduct={
-        name:req.body.name,
-        price:req.body.price,
-        qty:req.body.qty,
-        info:req.body.info,
-        image:req.body.image
-    }
-    
-    //console.log(newProduct)
-    //product is Exists or not ?
-    let product=await Product.findOne({name:newProduct.name})
-    if(product){
-        return resp.status(401).json({msg:"Product Already exists"})
-    }
-    product=new Product(newProduct);
-    //console.log(product)
-    await product.save();
-    resp.status(200).json({mgs:"Product created successfully",product:product})
-})
-
-/*
-URL: http://127.0.0.1:8080/products/read
-Method:GET
-Required Fields:No
-
-*/
-router.get("/read/",async(req,resp)=>{
-    let products=await Product.find();
-    resp.status(200).json(products)
-})
-
-
-
 /*
     URL: http://127.0.0.1:8080/products/update/663a163727afa4f6fa54e81a
-    Method :PUT
-    required Field:name,price,info,image,qty
 */
 
 router.put("/update/:id",async(req,resp)=>{
@@ -81,7 +39,7 @@ router.put("/update/:id",async(req,resp)=>{
     Method:Delete
     required Fields:None
 */
-router.delete("/delete/:id", async(req,resp)=>{
+router.delete("/:id", async(req,resp)=>{
     let productId=req.params.id
     //verify product exits or not 
     let product=await Product.findById(productId)
--- ProductCRUD/routes/productRouter-3.1.js
+++ ProductCRUD/routes/productRouter-3.1.js
@@ -2,6 +2,17 @@ import express from 'express'
 import Product from '../model/Product.js'
 let router = express.Router()
 
+/*
+URL: http://127.0.0.1:8080/products/all
+Method:GET
+Required Fields:No
+
+*/
+router.get("/all",async(req,resp)=>{
+    let products=await Product.find();
+    resp.status(200).json(products)
+})
+
 
 /*
     URL: http://127.0.0.1:8080/products/create
@@ -9,24 +20,27 @@ let router = express.Router()
     Required Fields: name,price,qty,info,image
 */
 router.post("/create", async(req,resp)=>{
-    let newProduct={
+    /* let newProduct={
         name:req.body.name,
         price:req.body.price,
         qty:req.body.qty,
         info:req.body.info,
         image:req.body.image
-    }
+    } */
+    let {name,price,qty,info,image}=req.body
     
     //console.log(newProduct)
     //product is Exists or not ?
-    let product=await Product.findOne({name:newProduct.name})
+    let product=await Product.findOne({name:name})
+    console.log(product)
     if(product){
         return resp.status(401).json({msg:"Product Already exists"})
     }
-    product=new Product(newProduct);
+    product=new Product({name,price,qty,info,image});
     //console.log(product)
     await product.save();
     resp.status(200).json({mgs:"Product created successfully",product:product})
+
 })
 
 export default router
